15b893ce7ff8bccc5599912fb7c79c26eff0ec42
a18ismca <a18ismca@student.his.se>
2022-06-20 22:43:29 +0200

    Added code and comments to the Subtraction and Multiplication methods

--- LXC_Assignment1/Program.cs
+++ LXC_Assignment1/Program.cs
@@ -1,7 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
-Addition();
+Subtraction();
 
+
+// The method below allows the user to enter two different integers and sum them. 
 void Addition()
 {
     Console.Write("Enter first int: ");
@@ -17,16 +19,39 @@ void Addition()
     Console.Write("The sum of the entered integers are " + sum);
 }
 
+// The method below allows the user to enter two different integers and subtract the first int with the second int. 
 void Subtraction()
 {
+    Console.Write("Enter first int: ");
 
+    int first = Convert.ToInt32(Console.ReadLine());
+
+    Console.Write("Enter second int: ");
+
+    int second = Convert.ToInt32(Console.ReadLine());
+
+    int sum = first - second;
+
+    Console.Write("The sum of the entered integers are " + sum);
 }
 
+// The method below allows the user to enter two different integers and multiply them with each other. 
 void Multiplication() 
-{ 
+{
+    Console.Write("Enter first int: ");
 
+    int first = Convert.ToInt32(Console.ReadLine());
+
+    Console.Write("Enter second int: ");
+
+    int second = Convert.ToInt32(Console.ReadLine());
+
+    int sum = first * second;
+
+    Console.Write("The sum of the entered integers are " + sum);
 }
 
+// The method below allows the user to enter two different integers and sum them. 
 void Division() 
 { 
 }
