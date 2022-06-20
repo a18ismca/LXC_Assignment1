06bb5bd57510176647535f18de867827601e4d7b
a18ismca <a18ismca@student.his.se>
2022-06-20 22:34:41 +0200

    Setup of proj.

--- LXC_Assignment1/Program.cs
+++ LXC_Assignment1/Program.cs
@@ -1,3 +1,32 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+Addition();
 
+void Addition()
+{
+    Console.Write("Enter first int: ");
+
+    int first = Convert.ToInt32(Console.ReadLine());
+
+    Console.Write("Enter second int: ");
+
+    int second = Convert.ToInt32(Console.ReadLine());
+
+    int sum = first + second;
+
+    Console.Write("The sum of the entered integers are " + sum);
+}
+
+void Subtraction()
+{
+
+}
+
+void Multiplication() 
+{ 
+
+}
+
+void Division() 
+{ 
+}
