174ac811985eef341ce7e6dc6ce0f6459cf18731
a18ismca <ismetcatovic@hotmail.se>
2022-06-21 11:08:50 +0200

    Added a menu method where the user can chose which algorithmic operation to use. The user can also decide if it wants to return to the menu or quit, after the user have executed one of the algorithms.

--- LXC_Assignment1/Program.cs
+++ LXC_Assignment1/Program.cs
@@ -1,22 +1,21 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Subtraction();
-
 
 // The method below allows the user to enter two different integers and sum them. 
 void Addition()
 {
     Console.Write("Enter first int: ");
 
-    int first = Convert.ToInt32(Console.ReadLine());
+    double first = Convert.ToDouble(Console.ReadLine());
 
     Console.Write("Enter second int: ");
 
-    int second = Convert.ToInt32(Console.ReadLine());
+    double second = Convert.ToDouble(Console.ReadLine());
 
-    int sum = first + second;
+    double sum = first + second;
 
-    Console.Write("The sum of the entered integers are " + sum);
+    Console.WriteLine("The sum of the entered integers are " + sum);
+
+    Continue();
 }
 
 // The method below allows the user to enter two different integers and subtract the first int with the second int. 
@@ -24,15 +23,17 @@ void Subtraction()
 {
     Console.Write("Enter first int: ");
 
-    int first = Convert.ToInt32(Console.ReadLine());
+    double first = Convert.ToDouble(Console.ReadLine());
 
     Console.Write("Enter second int: ");
 
-    int second = Convert.ToInt32(Console.ReadLine());
+    double second = Convert.ToDouble(Console.ReadLine());
 
-    int sum = first - second;
+    double sum = first - second;
 
-    Console.Write("The sum of the entered integers are " + sum);
+    Console.WriteLine("The sum of the entered integers are " + sum);
+
+    Continue();
 }
 
 // The method below allows the user to enter two different integers and multiply them with each other. 
@@ -40,15 +41,17 @@ void Multiplication()
 {
     Console.Write("Enter first int: ");
 
-    int first = Convert.ToInt32(Console.ReadLine());
+    double first = Convert.ToDouble(Console.ReadLine());
 
     Console.Write("Enter second int: ");
 
-    int second = Convert.ToInt32(Console.ReadLine());
+    double second = Convert.ToDouble(Console.ReadLine());
 
-    int sum = first * second;
+    double sum = first * second;
 
-    Console.Write("The faction of the entered integers are " + sum);
+    Console.WriteLine("The faction of the entered integers are " + sum);
+
+    Continue();
 }
 
 // The method below allows the user to enter two different integers and sum them. 
@@ -56,23 +59,77 @@ void Division()
 {
     Console.Write("Enter first int: ");
 
-    int first = Convert.ToInt32(Console.ReadLine());
+    double first = Convert.ToDouble(Console.ReadLine());
 
     Console.Write("Enter second int: ");
 
-    int second = Convert.ToInt32(Console.ReadLine());
+    double second = Convert.ToDouble(Console.ReadLine());
 
-    if(second != 0)
+    if (second != 0)
     {
-        int sum = first / second;
-        Console.Write("The faction of the entered integers are " + sum);
+        double sum = first / second;
+        Console.WriteLine("The faction of the entered integers are " + sum);
     }
     else
     {
         Console.WriteLine("The second int must not be 0. Try again.");
+        Division();
     }
-   
-    
+    Continue();
+}
 
+void Menu()
+{
+
+
+        Console.WriteLine
+            ("Welcome to the calculator menu!" + "\n" +
+            "Press 1 to add." + "\n" +
+            "Press 2 to subtract" + "\n" +
+            "Press 3 to multiply." + "\n" +
+            "Press 4 to divide");
+
+        int numPressed = Convert.ToInt32(Console.ReadLine());
+
+    Console.Clear();
+    switch (numPressed)
+        {
+        
+            case 1:
+                Addition();
+                break;
+            case 2:
+                Subtraction();
+                break;
+            case 3:
+                Multiplication();
+                break;
+            case 4:
+                Division();
+                break;
+            default: 
+            Console.WriteLine("Nope."); 
+            break;
+    }
 
 }
+
+bool Continue()
+{
+    Console.WriteLine("Press any key to return to the menu. Press Q to quit.");
+
+    
+        if (Console.ReadKey().Key == ConsoleKey.Q)
+        {
+        Console.Clear();
+        return false;
+        
+        }
+
+    Console.Clear();
+    Menu();
+
+    return true;
+}
+
+Menu();
