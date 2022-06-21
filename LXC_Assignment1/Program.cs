4c3cf727d89d6b1520a0f8ce2fc54a7d7af0f5f4
a18ismca <ismetcatovic@hotmail.se>
2022-06-21 13:09:17 +0200

    Changed general info and made some minor changes to the calc methods.

--- LXC_Assignment1/Program.cs
+++ LXC_Assignment1/Program.cs
@@ -1,5 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+
+
+// Use the menu method.
+    Menu();
+
+
 // The method below allows the user to enter two different integers and receive the sum of them. 
 void Addition()
 {
@@ -13,8 +19,7 @@ void Addition()
 
     double sum = first + second;
 
-    Console.WriteLine("The sum of the entered integers are " + sum);
-
+    Console.WriteLine(first + " + " + second + " = " + sum);
     Continue();
 }
 
@@ -29,9 +34,9 @@ void Subtraction()
 
     double second = Convert.ToDouble(Console.ReadLine());
 
-    double sum = first - second;
+    double difference = first - second;
 
-    Console.WriteLine("The sum of the entered integers are " + sum);
+    Console.WriteLine(first + " - " + second + " = " + difference);
 
     Continue();
 }
@@ -47,9 +52,9 @@ void Multiplication()
 
     double second = Convert.ToDouble(Console.ReadLine());
 
-    double sum = first * second;
+    double product = first * second;
 
-    Console.WriteLine("The faction of the entered integers are " + sum);
+    Console.WriteLine(first + " * " + second + " = " + product);
 
     Continue();
 }
@@ -57,24 +62,25 @@ void Multiplication()
 // The method below allows the user to receive the fraction of the numerator and the denomirator. 
 void Division() 
 {
-    Console.Write("Enter first int: ");
+    Console.Write("Enter numerator: ");
 
     double first = Convert.ToDouble(Console.ReadLine());
 
-    Console.Write("Enter second int: ");
+    Console.Write("Enter denomirator: ");
 
     double second = Convert.ToDouble(Console.ReadLine());
 
     if (second != 0)
     {
-        double sum = first / second;
-        Console.WriteLine("The fraction of the entered integers are " + sum);
+        double fraction = first / second;
+        Console.WriteLine(first + " / " + second + " = " + fraction);
     }
     else
     {
         Console.WriteLine("The second int must not be 0. Try again.");
         Division();
     }
+
     Continue();
 }
 
@@ -88,17 +94,18 @@ void Menu()
             "Press 1 to calculate the sum of two integers." + "\n" +
             "Press 2 to calculate the difference of two integers." + "\n" +
             "Press 3 to calculate the product of two integers." + "\n" +
-            "Press 4 to calculate the fraction of the numerator and the denomirator.");
+            "Press 4 to calculate the fraction of the numerator and the denomirator." + "\n" +
+            "Press 5 to quit." + "\n" +
+            "When pressing one of the keys above, press ENTER to move on to the desired action.");
 
     
-        int numPressed = Convert.ToInt32(Console.ReadLine());
+    int numPressed = Convert.ToInt32(Console.ReadLine());
 
-    // Clear the console window when pressing a button.
+    // Clear the console window when pressing on a button.
 
     Console.Clear();
     switch (numPressed)
-        {
-        
+    { 
             case 1:
                 Addition();
                 break;
@@ -111,10 +118,15 @@ void Menu()
             case 4:
                 Division();
                 break;
+            case 5:
+                Environment.Exit(0);
+                break;
             default: 
-            Console.WriteLine("Nope."); 
+            Console.WriteLine("The number entered does not contain anything"); 
             break;
     }
+
+  
 
 }
 
@@ -126,7 +138,7 @@ bool Continue()
     
         if (Console.ReadKey().Key == ConsoleKey.Q)
         {
-        Console.Clear();
+       
         return false;
         
         }
@@ -137,4 +149,3 @@ bool Continue()
     return true;
 }
 
-Menu();
